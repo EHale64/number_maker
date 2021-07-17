590c5b6a54cb53c689f64d50aba42f38a891a808
Eric Hale <eric_hale@comcast.net>
2021-07-16 19:46:53 -0600

    first solution

--- lib/phone_numbers.rb
+++ lib/phone_numbers.rb
@@ -1,1 +1,16 @@
-class 
+class NumberMaker
+    def create_phone_number(numbers)
+        phone = []
+        first = numbers[0,3]
+        second = numbers[3,3]
+        third = numbers[6,4]
+        first.unshift('(')
+        first.push(')')
+        second.unshift(' ')
+        third.unshift('-')
+        phone << first
+        phone << second
+        phone << third
+        phone.join
+    end
+end
